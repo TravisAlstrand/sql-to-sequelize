6f7e5639a2f5ce5d862e2c6ae5acf1f4e1b731ff
Travis Alstrand <travisalstrand@gmail.com>
2023-07-06 10:55:18 -0700

    updated with solutions from ChatGPT

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
-'use strict';
+"use strict";
 
 // load modules
-const express = require('express');
-const morgan = require('morgan');
-const { sequelize } = require('./models/index');
-const router = require('./routes/index');
+const express = require("express");
+const morgan = require("morgan");
+const { sequelize } = require("./models/index");
+const router = require("./routes/index");
 
 // variable to enable global error logging
-const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+const enableGlobalErrorLogging =
+  process.env.ENABLE_GLOBAL_ERROR_LOGGING === "true";
 
 // create the Express app
 const app = express();
 app.use(express.json());
-
 // setup morgan which gives us http request logging
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/', router);
+app.use("/", router);
 
-// const sequelize = new Sequelize({
-//   dialect: 'sqlite',
-//   storage: 'blogApp.db'
-// });
-
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Connection to database successful!')
-  }).catch((err) => {
-    console.error('Connection to database unsuccessful!', err);
+    console.log("Connection to database successful!");
+  })
+  .catch((err) => {
+    console.error("Connection to database unsuccessful!", err);
   });
 
 // send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Route Not Found',
+    message: "Route Not Found",
   });
 });
 
@@ -50,9 +47,9 @@ app.use((err, req, res, next) => {
 });
 
 // set our port
-app.set('port', process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000);
 
 // start listening on our port
-const server = app.listen(app.get('port'), () => {
+const server = app.listen(app.get("port"), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,37 @@ const router = express.Router();
 const { User, BlogPost } = require("../models");
 const { asyncHandler } = require("../middleware/asyncHandler");
 const { sequelize } = require("../models/index");
+const { literal } = require("sequelize");
 
-// router.get('/allBlogs', asyncHandler(async (req, res) => {
-//   try {
-//     const results = await sequelize.query('SELECT * FROM BlogPosts JOIN Users ON BlogPosts.userId = Users.id');
-//     res.json(results);
-//   } catch (error) {
-//     res.status(500).json({ error: `${error}` });
-//   }
-// }));
+// router.get(
+//   "/all-blogs",
+//   asyncHandler(async (req, res) => {
+//     try {
+//       const results = await sequelize.query(`
+//         SELECT BlogPosts.id, title, body, userId, Users.username
+//         FROM BlogPosts
+//         JOIN Users ON BlogPosts.userId = Users.id
+//       `);
+//       res.json(results);
+//     } catch (error) {
+//       res.status(500).json({ error: `${error}` });
+//     }
+//   })
+// );
 
 router.get(
-  "/allBlogs",
+  "/all-blogs",
   asyncHandler(async (req, res) => {
     try {
       const results = await BlogPost.findAll({
-        attributes: {
-          exclude: ["createdAt", "updatedAt"],
-        },
-        include: {
-          model: User,
-          as: "author",
-          attributes: {
-            exclude: ["password", "createdAt", "updatedAt"],
+        attributes: ["id", "title", "body", "userId"],
+        include: [
+          {
+            model: User,
+            as: "author",
+            attributes: ["username"],
           },
-        },
+        ],
       });
       res.json(results);
     } catch (error) {
@@ -36,21 +42,94 @@ router.get(
   })
 );
 
+// router.get(
+//   "/brian",
+//   asyncHandler(async (req, res) => {
+//     try {
+//       const results = await sequelize.query(`
+//         SELECT BlogPosts.id, title, body, userId, Users.username
+//         FROM BlogPosts
+//         JOIN Users ON BlogPosts.userId = Users.id
+//         Where Users.username = 'Brian'
+//       `);
+//       res.json(results);
+//     } catch (error) {
+//       res.status(500).json({ error: `${error}` });
+//     }
+//   })
+// );
+
 router.get(
-  "/slackers",
+  "/brian",
   asyncHandler(async (req, res) => {
     try {
-      const results = await sequelize.query(`
-    SELECT id, title, body
-    FROM BlogPosts AS bp
-    JOIN(
-        SELECT userId, COUNT(*) AS postCount
-          FROM BlogPosts
-          GROUP BY userId
-          ORDER BY postCount ASC
-          LIMIT 1
-      ) AS userPosts ON bp.userId = userPosts.userId
-    `);
+      const results = await BlogPost.findAll({
+        attributes: ["id", "title", "body", "userId"],
+        include: [
+          {
+            model: User,
+            as: "author",
+            attributes: ["username"],
+            where: {
+              username: "Brian",
+            },
+          },
+        ],
+      });
+      res.json(results);
+    } catch (error) {
+      res.status(500).json({ error: `${error}` });
+    }
+  })
+);
+
+// router.get(
+//   "/slacker",
+//   asyncHandler(async (req, res) => {
+//     try {
+//       const results = await sequelize.query(`
+//         SELECT bp.id, bp.title, bp.body, bp.userId, u.username
+//         FROM BlogPosts AS bp
+//         JOIN(
+//             SELECT userId, COUNT(*) AS postCount
+//             FROM BlogPosts
+//             GROUP BY userId
+//             ORDER BY postCount ASC
+//             LIMIT 1
+//         ) AS userPosts ON bp.userId = userPosts.userId
+//         JOIN Users AS u ON bp.userId = u.id;
+//       `);
+//       res.json(results);
+//     } catch (error) {
+//       res.status(500).json({ error: `${error}` });
+//     }
+//   })
+// );
+
+router.get(
+  "/slacker",
+  asyncHandler(async (req, res) => {
+    try {
+      const results = await BlogPost.findAll({
+        attributes: ["id", "title", "body", "userId"],
+        include: [
+          {
+            model: User,
+            as: "author",
+            attributes: ["username"],
+          },
+        ],
+        where: {},
+        order: [
+          [
+            literal(
+              "(SELECT COUNT(*) FROM BlogPosts WHERE BlogPosts.userId = author.id)"
+            ),
+            "ASC",
+          ],
+        ],
+        limit: 1,
+      });
       res.json(results);
     } catch (error) {
       res.status(500).json({ error: `${error}` });
